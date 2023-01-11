ced7ce622ca12b977d7dd498d13f9699d3102ae2
longlostjames <chris.walden@ncas.ac.uk>
2023-01-11 11:30:29 +0000

    main

--- galileo_quicklooks_4h.py
+++ galileo_quicklooks_4h.py
@@ -37,34 +37,6 @@ galileo_raw_path = '/radar/radar-galileo/raw'
 
 data_datetime = datetime.now()
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:h:", ["day=","hour="])
-except getopt.GetoptError as err:
-    # print help information and exit:
-    print(err)  # will print something like "option -a not recognized
-    sys.exit(2)
-
-
-for o, a in opts:
-    if o == "-d":
-        day = a
-        data_datetime = datetime.strptime(day,"%Y%m%d")
-    elif o in ("-h"):
-        hour_end = int(a)
-        print(hour_end)
-        data_datetime.replace(hour=hour_end,minute=0,second=0,microsecond=0)
-        print(data_datetime)
-    else:
-            assert False, "unhandled option"
-
-
-dt1 = data_datetime
-dt0 = data_datetime - timedelta(hours=4)
-datestr1 = dt1.strftime('%Y%m%d')
-datestr0 = dt0.strftime('%Y%m%d')
-
-print(dt1);
-print(dt0);
 
 mpl.use('Agg')
 
@@ -122,234 +94,269 @@ def make_cmap(colors, position=None, bit=False):
     return cmap
 
 
-with open('hoganjet.csv') as f:
-    hoganjet = [tuple(map(int, i.split(','))) for i in f]
+def main():
 
-cmap_hoganjet = make_cmap(hoganjet, bit=True)
-
-# LOCATE GALILEO FILES FOR SELECTED DATE
-
-dateyr = datestr1[0:4]
-dateym = datestr1[0:6]
-user = getpass.getuser()
-
-os.chdir(os.path.join(galileo_raw_path, datestr0))
-rawfiles94 = [os.path.join(galileo_raw_path, datestr0, f)
-              for f in glob.glob('*{}*raw.nc'.format(datestr0))]
-
-os.chdir(os.path.join(galileo_raw_path, datestr1))
-rawfiles94 += [os.path.join(galileo_raw_path, datestr1, f)
-               for f in glob.glob('*{}*raw.nc'.format(datestr1))]
+    try:
+    opts, args = getopt.getopt(sys.argv[1:], "d:h:", ["day=", "hour="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+    print(err)  # will print something like "option -a not recognized
+    sys.exit(2)
 
 
-print(rawfiles94)
-
-output = set()
-for x in rawfiles94:
-    output.add(x)
-print(output)
-
-rawfiles94 = list(output)
-
-print(rawfiles94)
-
-figpath = os.path.join(quicklook_base_path, dateyr, dateym, datestr1)
-
-istartfile = 0
-iendfile = -1
-
-DS0 = nc4.Dataset(rawfiles94[istartfile], 'r')
-dtime_start = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
-
-DS1 = nc4.Dataset(rawfiles94[iendfile], 'r')
-dtime_end = cftime.num2pydate(DS1['time'][:], DS1['time'].units)
-
-# Fix day rollover problem
-if dtime_end[-1] < dtime_end[-2]:
-    dtime_end[-1] = dtime_end[-1]+timedelta(days=1)
-
-dt_min = dt0
-dt_max = dt1
-
-# dt_min = dt_min.replace(second=0, microsecond=0)
-
-DS0.close()
-DS1.close()
-
-print(dt_min, dt_max)
-
-fig, axs = plt.subplots(5, 1, figsize=(12, 15), constrained_layout=True)
-fig.set_constrained_layout_pads(
-    w_pad=2 / 72, h_pad=2 / 72, hspace=0.2, wspace=0.2)
-
-hmax = 12
-
-axs[0].set_xlim(dt_min, dt_max)
-axs[0].set_ylim(0, hmax)
-axs[1].set_xlim(dt_min, dt_max)
-axs[1].set_ylim(0, hmax)
-axs[2].set_xlim(dt_min, dt_max)
-axs[2].set_ylim(0, hmax)
-axs[3].set_xlim(dt_min, dt_max)
-axs[3].set_ylim(0, hmax)
-axs[4].set_xlim(dt_min, dt_max)
-axs[4].set_ylim(0, hmax)
-
-DS0 = nc4.Dataset(rawfiles94[istartfile], 'r', format='NETCDF4_CLASSIC')
-DS0.set_auto_mask(True)
-
-print(DS0)
-
-dtime0 = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
-if dtime0[-1] < dtime0[-2]:
-    dtime0[-1] = dtime0[-1]+timedelta(days=1)
-
-myFmt = mdates.DateFormatter('%H:%M')
-
-nray = DS0['time'].shape[0]
-
-print(nray)
-
-rng = DS0['range'][:]
-
-ngate = DS0['range'].shape[0]
-
-print(ngate)
-print(rng.shape)
-ZED_HCnew = DS0['ZED_HC'][:, :]
-print(ZED_HCnew.shape)
-
-rng = DS0['range'][:]
-
-ngate = DS0['range'].shape[0]
-
-range_offset = -389.7301954
-drng = range_offset-DS0['range'].range_offset
+    for o, a in opts:
+        if o == "-d":
+            day = a
+            data_datetime = datetime.strptime(day, "%Y%m%d")
+        elif o in ("-h"):
+            hour_end = int(a)
+            print(hour_end)
+            data_datetime.replace(hour=hour_end, minute=0, second=0, microsecond=0)
+            print(data_datetime)
+        else:
+            assert False, "unhandled option"
 
 
-ZED_HCnew = ZED_HCnew+20. * \
-    np.log10(rng[None, :])-20.*np.log10(rng[None, :]+drng)
+    dt1 = data_datetime
+    dt0 = data_datetime - timedelta(hours=4)
+    datestr1 = dt1.strftime('%Y%m%d')
+    datestr0 = dt0.strftime('%Y%m%d')
 
-range_km = (rng+drng)/1000.
+    print(dt1)
+    print(dt0)
 
-gate_edges = range_km - 29.9792458/1000.
-gate_edges = np.append(gate_edges, gate_edges[-1]+2*29.9792458/1000.)
+    with open('hoganjet.csv') as f:
+        hoganjet = [tuple(map(int, i.split(','))) for i in f]
 
-spw_plotmin = np.sqrt(1e-3)
-spw_plotmax = np.sqrt(10.)
+    cmap_hoganjet = make_cmap(hoganjet, bit=True)
 
-axs[0].xaxis.set_major_formatter(myFmt)
-h0 = axs[0].pcolormesh(dtime0[:], gate_edges, ZED_HCnew.transpose(
-), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
-titlestr = "Chilbolton W-band Galileo Radar: "+datestr1
-axs[0].set_title(titlestr)
-cb0 = plt.colorbar(h0, ax=axs[0], orientation='vertical')
-cb0.ax.set_ylabel("ZED_HC (dB)")
-axs[0].grid(True)
-axs[0].set_xlabel('Time (UTC)')
-axs[0].set_ylabel('Height (km)')
+    # LOCATE GALILEO FILES FOR SELECTED DATE
 
-axs[1].xaxis.set_major_formatter(myFmt)
-h1 = axs[1].pcolormesh(dtime0[:], gate_edges, DS0['VEL_HC'][:, :].transpose(
-), vmin=-5, vmax=5, cmap=cmap_hoganjet, shading='auto')
-cb1 = plt.colorbar(h1, ax=axs[1], orientation='vertical')
-cb1.ax.set_ylabel("VEL_HC (m$s^{-1}$)")
-axs[1].grid(True)
-axs[1].set_xlabel('Time (UTC)')
-axs[1].set_ylabel('Height (km)')
+    dateyr = datestr1[0:4]
+    dateym = datestr1[0:6]
+    user = getpass.getuser()
 
-try:
-    axs[2].xaxis.set_major_formatter(myFmt)
-    h2 = axs[2].pcolormesh(dtime0[:], gate_edges, DS0['LDR_HC'][:, :].transpose(
-    ), vmin=-35, vmax=5, cmap=cmap_hoganjet, shading='auto')
-    cb2 = plt.colorbar(h2, ax=axs[2], orientation='vertical')
-    cb2.ax.set_ylabel("LDR_HC (dB)")
-    axs[2].grid(True)
-    axs[2].set_xlabel('Time (UTC)')
-    axs[2].set_ylabel('Height (km)')
-except:
-    print("No LDR_HC")
+    os.chdir(os.path.join(galileo_raw_path, datestr0))
+    rawfiles94 = [os.path.join(galileo_raw_path, datestr0, f)
+                for f in glob.glob('*{}*raw.nc'.format(datestr0))]
 
-try:
-    axs[3].xaxis.set_major_formatter(myFmt)
-    h3 = axs[3].pcolormesh(dtime0[:], gate_edges, DS0['SPW_HC'][:, :].transpose(
-    ), norm=colors.LogNorm(vmin=spw_plotmin, vmax=spw_plotmax), cmap=cmap_hoganjet, shading='auto')
-    cb3 = plt.colorbar(h3, ax=axs[3], orientation='vertical')
-    cb3.ax.set_ylabel("SPW_HC (m$s^{-1}$)")
-    axs[3].grid(True)
-    axs[3].set_xlabel('Time (UTC)')
-    axs[3].set_ylabel('Height (km)')
-except:
-    print("No SPW_HC")
-
-try:
-    ZED_XHCnew = DS0['ZED_XHC'][:, :]
-    ZED_XHCnew = ZED_XHCnew+20. * \
-        np.log10(rng[None, :])-20.*np.log10(rng[None, :]-drng)
-except:
-    print("No ZED_XHC")
-    ZED_XHCnew = np.empty([nray, ngate])
-
-axs[4].xaxis.set_major_formatter(myFmt)
-h4 = axs[4].pcolormesh(dtime0[:], gate_edges, ZED_XHCnew[:, :].transpose(
-), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
-cb4 = plt.colorbar(h4, ax=axs[4], orientation='vertical')
-cb4.ax.set_ylabel("ZED_XHC (dBZ)")
-axs[4].grid(True)
-axs[4].set_xlabel('Time (UTC)')
-axs[4].set_ylabel('Height (km)')
+    os.chdir(os.path.join(galileo_raw_path, datestr1))
+    rawfiles94 += [os.path.join(galileo_raw_path, datestr1, f)
+                for f in glob.glob('*{}*raw.nc'.format(datestr1))]
 
 
-DS0.close()
+    print(rawfiles94)
 
-for file in rawfiles94[istartfile+1:]:
-    DS0 = nc4.Dataset(file, 'r', format='NETCDF4_CLASSIC')
+    output = set()
+    for x in rawfiles94:
+        output.add(x)
+    print(output)
+
+    rawfiles94 = list(output)
+
+    print(rawfiles94)
+
+    figpath = os.path.join(quicklook_base_path, dateyr, dateym, datestr1)
+
+    istartfile = 0
+    iendfile = -1
+
+    DS0 = nc4.Dataset(rawfiles94[istartfile], 'r')
+    dtime_start = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
+
+    DS1 = nc4.Dataset(rawfiles94[iendfile], 'r')
+    dtime_end = cftime.num2pydate(DS1['time'][:], DS1['time'].units)
+
+    # Fix day rollover problem
+    if dtime_end[-1] < dtime_end[-2]:
+        dtime_end[-1] = dtime_end[-1]+timedelta(days=1)
+
+    dt_min = dt0
+    dt_max = dt1
+
+    # dt_min = dt_min.replace(second=0, microsecond=0)
+
+    DS0.close()
+    DS1.close()
+
+    print(dt_min, dt_max)
+
+    fig, axs = plt.subplots(5, 1, figsize=(12, 15), constrained_layout=True)
+    fig.set_constrained_layout_pads(
+        w_pad=2 / 72, h_pad=2 / 72, hspace=0.2, wspace=0.2)
+
+    hmax = 12
+
+    axs[0].set_xlim(dt_min, dt_max)
+    axs[0].set_ylim(0, hmax)
+    axs[1].set_xlim(dt_min, dt_max)
+    axs[1].set_ylim(0, hmax)
+    axs[2].set_xlim(dt_min, dt_max)
+    axs[2].set_ylim(0, hmax)
+    axs[3].set_xlim(dt_min, dt_max)
+    axs[3].set_ylim(0, hmax)
+    axs[4].set_xlim(dt_min, dt_max)
+    axs[4].set_ylim(0, hmax)
+
+    DS0 = nc4.Dataset(rawfiles94[istartfile], 'r', format='NETCDF4_CLASSIC')
     DS0.set_auto_mask(True)
 
-    if nray > 0:
+    print(DS0)
 
-        dtime0 = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
-        if dtime0[-1] < dtime0[-2]:
-            dtime0[-1] = dtime0[-1]+timedelta(days=1)
+    dtime0 = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
+    if dtime0[-1] < dtime0[-2]:
+        dtime0[-1] = dtime0[-1]+timedelta(days=1)
 
-        rng = DS0['range'][:]
+    myFmt = mdates.DateFormatter('%H:%M')
 
-        drng = range_offset-DS0['range'].range_offset
+    nray = DS0['time'].shape[0]
 
-        range_km = (rng+drng)/1000.
+    print(nray)
 
-        gate_edges = range_km - 29.9792458/1000.
-        gate_edges = np.append(gate_edges, gate_edges[-1]+2*29.9792458/1000.)
+    rng = DS0['range'][:]
 
-        ZED_HCnew = DS0['ZED_HC'][:, :]+20. * \
-            np.log10(rng[None, :])-20.*np.log10(rng[None, :]+drng)
+    ngate = DS0['range'].shape[0]
 
-        try:
-            axs[1].pcolormesh(dtime0[:], gate_edges, DS0['VEL_HC'][:, :].transpose(
-            ), vmin=-5, vmax=5, cmap=cmap_hoganjet, shading='auto')
-            axs[2].pcolormesh(dtime0[:], gate_edges, DS0['LDR_HC'][:, :].transpose(
-            ), vmin=-35, vmax=5, cmap=cmap_hoganjet, shading='auto')
-            axs[0].pcolormesh(dtime0[:], gate_edges, ZED_HCnew.transpose(
-            ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
-            axs[3].pcolormesh(dtime0[:], gate_edges, DS0['SPW_HC'][:, :].transpose(), norm=colors.LogNorm(
-                vmin=spw_plotmin, vmax=spw_plotmax), cmap=cmap_hoganjet, shading='auto')
+    print(ngate)
+    print(rng.shape)
+    ZED_HCnew = DS0['ZED_HC'][:, :]
+    print(ZED_HCnew.shape)
 
-            ZED_XHCnew = DS0['ZED_XHC'][:, :]
-            ZED_XHCnew = ZED_XHCnew+20. * \
+    rng = DS0['range'][:]
+
+    ngate = DS0['range'].shape[0]
+
+    range_offset = -389.7301954
+    drng = range_offset-DS0['range'].range_offset
+
+
+    ZED_HCnew = ZED_HCnew+20. * \
+        np.log10(rng[None, :])-20.*np.log10(rng[None, :]+drng)
+
+    range_km = (rng+drng)/1000.
+
+    gate_edges = range_km - 29.9792458/1000.
+    gate_edges = np.append(gate_edges, gate_edges[-1]+2*29.9792458/1000.)
+
+    spw_plotmin = np.sqrt(1e-3)
+    spw_plotmax = np.sqrt(10.)
+
+    axs[0].xaxis.set_major_formatter(myFmt)
+    h0 = axs[0].pcolormesh(dtime0[:], gate_edges, ZED_HCnew.transpose(
+    ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
+    titlestr = "Chilbolton W-band Galileo Radar: "+datestr1
+    axs[0].set_title(titlestr)
+    cb0 = plt.colorbar(h0, ax=axs[0], orientation='vertical')
+    cb0.ax.set_ylabel("ZED_HC (dB)")
+    axs[0].grid(True)
+    axs[0].set_xlabel('Time (UTC)')
+    axs[0].set_ylabel('Height (km)')
+
+    axs[1].xaxis.set_major_formatter(myFmt)
+    h1 = axs[1].pcolormesh(dtime0[:], gate_edges, DS0['VEL_HC'][:, :].transpose(
+    ), vmin=-5, vmax=5, cmap=cmap_hoganjet, shading='auto')
+    cb1 = plt.colorbar(h1, ax=axs[1], orientation='vertical')
+    cb1.ax.set_ylabel("VEL_HC (m$s^{-1}$)")
+    axs[1].grid(True)
+    axs[1].set_xlabel('Time (UTC)')
+    axs[1].set_ylabel('Height (km)')
+
+    try:
+        axs[2].xaxis.set_major_formatter(myFmt)
+        h2 = axs[2].pcolormesh(dtime0[:], gate_edges, DS0['LDR_HC'][:, :].transpose(
+        ), vmin=-35, vmax=5, cmap=cmap_hoganjet, shading='auto')
+        cb2 = plt.colorbar(h2, ax=axs[2], orientation='vertical')
+        cb2.ax.set_ylabel("LDR_HC (dB)")
+        axs[2].grid(True)
+        axs[2].set_xlabel('Time (UTC)')
+        axs[2].set_ylabel('Height (km)')
+    except:
+        print("No LDR_HC")
+
+    try:
+        axs[3].xaxis.set_major_formatter(myFmt)
+        h3 = axs[3].pcolormesh(dtime0[:], gate_edges, DS0['SPW_HC'][:, :].transpose(
+        ), norm=colors.LogNorm(vmin=spw_plotmin, vmax=spw_plotmax), cmap=cmap_hoganjet, shading='auto')
+        cb3 = plt.colorbar(h3, ax=axs[3], orientation='vertical')
+        cb3.ax.set_ylabel("SPW_HC (m$s^{-1}$)")
+        axs[3].grid(True)
+        axs[3].set_xlabel('Time (UTC)')
+        axs[3].set_ylabel('Height (km)')
+    except:
+        print("No SPW_HC")
+
+    try:
+        ZED_XHCnew = DS0['ZED_XHC'][:, :]
+        ZED_XHCnew = ZED_XHCnew+20. * \
+            np.log10(rng[None, :])-20.*np.log10(rng[None, :]-drng)
+    except:
+        print("No ZED_XHC")
+        ZED_XHCnew = np.empty([nray, ngate])
+
+    axs[4].xaxis.set_major_formatter(myFmt)
+    h4 = axs[4].pcolormesh(dtime0[:], gate_edges, ZED_XHCnew[:, :].transpose(
+    ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
+    cb4 = plt.colorbar(h4, ax=axs[4], orientation='vertical')
+    cb4.ax.set_ylabel("ZED_XHC (dBZ)")
+    axs[4].grid(True)
+    axs[4].set_xlabel('Time (UTC)')
+    axs[4].set_ylabel('Height (km)')
+
+
+    DS0.close()
+
+    for file in rawfiles94[istartfile+1:]:
+        DS0 = nc4.Dataset(file, 'r', format='NETCDF4_CLASSIC')
+        DS0.set_auto_mask(True)
+
+        if nray > 0:
+
+            dtime0 = cftime.num2pydate(DS0['time'][:], DS0['time'].units)
+            if dtime0[-1] < dtime0[-2]:
+                dtime0[-1] = dtime0[-1]+timedelta(days=1)
+
+            rng = DS0['range'][:]
+
+            drng = range_offset-DS0['range'].range_offset
+
+            range_km = (rng+drng)/1000.
+
+            gate_edges = range_km - 29.9792458/1000.
+            gate_edges = np.append(gate_edges, gate_edges[-1]+2*29.9792458/1000.)
+
+            ZED_HCnew = DS0['ZED_HC'][:, :]+20. * \
                 np.log10(rng[None, :])-20.*np.log10(rng[None, :]+drng)
-            axs[4].pcolormesh(dtime0[:], gate_edges, ZED_XHCnew[:, :].transpose(
-            ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
-        except:
-            print("Problem with plot")
 
-        DS0.close()
+            try:
+                axs[1].pcolormesh(dtime0[:], gate_edges, DS0['VEL_HC'][:, :].transpose(
+                ), vmin=-5, vmax=5, cmap=cmap_hoganjet, shading='auto')
+                axs[2].pcolormesh(dtime0[:], gate_edges, DS0['LDR_HC'][:, :].transpose(
+                ), vmin=-35, vmax=5, cmap=cmap_hoganjet, shading='auto')
+                axs[0].pcolormesh(dtime0[:], gate_edges, ZED_HCnew.transpose(
+                ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
+                axs[3].pcolormesh(dtime0[:], gate_edges, DS0['SPW_HC'][:, :].transpose(), norm=colors.LogNorm(
+                    vmin=spw_plotmin, vmax=spw_plotmax), cmap=cmap_hoganjet, shading='auto')
 
-axs[0].grid(True)
-axs[1].grid(True)
-axs[2].grid(True)
-axs[3].grid(True)
-axs[4].grid(True)
+                ZED_XHCnew = DS0['ZED_XHC'][:, :]
+                ZED_XHCnew = ZED_XHCnew+20. * \
+                    np.log10(rng[None, :])-20.*np.log10(rng[None, :]+drng)
+                axs[4].pcolormesh(dtime0[:], gate_edges, ZED_XHCnew[:, :].transpose(
+                ), vmin=-40, vmax=40, cmap=cmap_hoganjet, shading='auto')
+            except:
+                print("Problem with plot")
 
-figfile = "radar-galileo_{}_last4hr.png".format(datestr1)
+            DS0.close()
 
-plt.savefig(os.path.join(figpath, figfile), dpi=200)
+    axs[0].grid(True)
+    axs[1].grid(True)
+    axs[2].grid(True)
+    axs[3].grid(True)
+    axs[4].grid(True)
+
+    figfile = "radar-galileo_{}_last4hr.png".format(datestr1)
+
+    plt.savefig(os.path.join(figpath, figfile), dpi=200)
+
+
+if __name__ == "__main__":
+    main()
